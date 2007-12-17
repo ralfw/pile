df6be562b7503ebff01e584c9c905de2ff8af396
info@ralfw.de <info@ralfw.de@b354594b-6b41-0410-b4e6-5db674d06026>
2007-12-17 12:03:30 +0000

    initial commit Demo.TextAssimilator

---
+++ source/Demo.TextAssimilator/TextAssimilator/TextAssimilator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextAssimilator
+{
+    public class TextAssimilator
+    {
+        public int Assimilate(string text)
+        {
+            return 0;
+        }
+
+        public string Regenerate(int index)
+        {
+            return "";
+        }
+    }
+}
